d257937263b0d5eb6240ef6d30bb5a79d14031a9
Danny Parsons <danny@aims.ac.za>
2021-08-19 09:28:04 +0100

    added nc argument to extract*, fld*, get_*, grid* functions

--- R/gridboxmin.R
+++ R/gridboxmin.R
@@ -14,6 +14,8 @@
 #'  in NetCDFv3 format (numeric). Default output is NetCDFv4.
 #'@param overwrite logical; should existing output file be overwritten?
 #'@param verbose logical; if TRUE, progress messages are shown
+#'@param nc Alternatively to `infile` you can instead specify the input as an
+#'  object of class `ncdf4` (as returned from `ncdf4::nc_open`).
 #'
 #'@return A NetCDF file including a time series of minima of selected grid boxes is written.
 #'@export
@@ -58,6 +60,7 @@
 #'
 #'unlink(c(file.path(tempdir(), "CMSAF_example_file.nc"), file.path(tempdir(), 
 #'  "CMSAF_example_file_gridboxmin.nc")))
-gridboxmin <- function(var, lonGrid, latGrid, infile, outfile, nc34 = 4, overwrite = FALSE, verbose = FALSE) {
-  gridboxx_wrapper(2, var, lonGrid, latGrid, infile, outfile, nc34, overwrite, verbose)
+gridboxmin <- function(var, lonGrid, latGrid, infile, outfile, nc34 = 4, overwrite = FALSE, verbose = FALSE,
+                       nc = NULL) {
+  gridboxx_wrapper(2, var, lonGrid, latGrid, infile, outfile, nc34, overwrite, verbose, nc = nc)
 }
