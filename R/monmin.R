2d4dc29e9eea1cba869af00f7973d125d8c0fc76
Danny Parsons <danny@aims.ac.za>
2021-08-19 10:27:45 +0100

    added nc argument to hour*, mer*, mon*, multi*, ncinfo, read*, run*, seas.anomaly functions

--- R/monmin.R
+++ R/monmin.R
@@ -12,6 +12,8 @@
 #'  in NetCDFv3 format (numeric). Default output is NetCDFv4.
 #'@param overwrite logical; should existing output file be overwritten?
 #'@param verbose logical; if TRUE, progress messages are shown
+#'@param nc Alternatively to `infile` you can instead specify the input as an
+#'  object of class `ncdf4` (as returned from `ncdf4::nc_open`).
 #'
 #'@return A NetCDF file including a time series of monthly minima is written.
 #'@export
@@ -55,6 +57,7 @@
 #'
 #'unlink(c(file.path(tempdir(),"CMSAF_example_file.nc"), 
 #'  file.path(tempdir(),"CMSAF_example_file_monmin.nc")))
-monmin <- function(var, infile, outfile, nc34 = 4, overwrite = FALSE, verbose = FALSE) {
-  monx_wrapper(2, var, infile, outfile, nc34, overwrite, verbose)
+monmin <- function(var, infile, outfile, nc34 = 4, overwrite = FALSE, verbose = FALSE,
+                   nc = NULL) {
+  monx_wrapper(2, var, infile, outfile, nc34, overwrite, verbose, nc = nc)
 }
